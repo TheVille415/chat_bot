06fde16a2d6ed9878a96d74c56fd8bf507f16b25
Jordan_Torres <jt.arontorres@gmail.com>
2020-09-22 19:52:29 -0700

    added if statement and print statement at top and imported random

--- chat_bot.py
+++ chat_bot.py
@@ -1,3 +1,12 @@
+import random
+print ("Hi! I am Chat Bot! I am a program designed to see how you are feeling.")
+user_response = input("Now tell me, are you feeling happy or sad? ")
+
 def get_bot_response(user_response):
     happyList = ["That's wonderful!", "Go you! Keep it up!", "These are good vibes"]
-    
+    sadList = ["Maybe you should go for a walk!", "A smoothie might cheer you up!", "Sending you positve and happy energy!"]
+
+    if user_response == "happy" or "Happy":
+        print(random.choice(happyList))
+    elif user_response == "sad" or "Sad":
+        print(random.choice(sadList))
