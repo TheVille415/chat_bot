f388125d7e165e446642c29bc8a9f5cab5e12f40
Jordan_Torres <jt.arontorres@gmail.com>
2020-09-22 20:59:16 -0700

    got the program working with happy and sad. will add more

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # chatBot.py
 # Python 3.7.3 64-bit
 
-This is an assignment where I had to make a chat bot.
+This is an assignment where I had to make a chat bot. 
 
-I will be taking the users input and giving them a response based off how they are feeling
+I will be taking the users input and giving them a response based off how they are feeling 
+
+If they say happy it returns a happy reponse, sad will return a sad response Anything else will return something encouraging still
+
+at the end it takes the users information they gave and will run until the user is done.
--- chat_bot.py
+++ chat_bot.py
@@ -1,12 +1,24 @@
-import random
-print ("Hi! I am Chat Bot! I am a program designed to see how you are feeling.")
-user_response = input("Now tell me, are you feeling happy or sad? ")
+from random import choice
 
 def get_bot_response(user_response):
     happyList = ["That's wonderful!", "Go you! Keep it up!", "These are good vibes"]
     sadList = ["Maybe you should go for a walk!", "A smoothie might cheer you up!", "Sending you positve and happy energy!"]
 
-    if user_response == "happy" or "Happy":
-        print(random.choice(happyList))
-    elif user_response == "sad" or "Sad":
-        print(random.choice(sadList))
+    if user_response == "happy":
+        return choice(happyList)
+    elif user_response == "sad":
+        return choice(sadList)
+    else:
+      print("I know you're feeling something but I cant tell what that is!")
+
+print ("Hi! I am Chat Bot! I am a program designed to see how you are feeling.")
+print("Are you feeling happy or sad? ")
+
+user_response = ""
+
+while True:
+  user_response = input("How are you feeling today?: ")
+  if user_response == 'done':
+    break
+  bot_response = get_bot_response(user_response)
+  print(bot_response)
